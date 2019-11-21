2467af30c0e7a8a3c4e09a7ca8b48dd1d65d079f
MukendiMputu <mputusam@live.com>
2019-11-20 23:07:37 -0600

    deleted test file
    Signed-off-by: MukendiMputu <mputusam@live.com>

--- src/App.js
+++ src/App.js
@@ -3,24 +3,11 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit the file <code>src/App.js</code> and save to reload.<br/>
-          The reload happens automatically.<br/>
-          Well then! Let's get the party started!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
     </div>
   );
 }
