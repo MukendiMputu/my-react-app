4652fc4a6a157862175fecf6544e932d6e8003b4
MukendiMputu <mukendi.mputu@tu-dortmund.de>
2019-11-20 22:12:32 -0600

    testing around with App.js
    Signed-off-by: MukendiMputu <mukendi.mputu@tu-dortmund.de>

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit the file <code>src/App.js</code> and save to reload.
+          Edit the file <code>src/App.js</code> and save to reload.<br/>
+          The reload happens automatically.
         </p>
         <a
           className="App-link"
