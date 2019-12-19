0b4c553ac8a5a8bb358eeec09d3a90d215d0085b
MukendiMputu <mputusam@live.com>
2019-12-19 10:34:13 -0600

    added a simple footer
    Signed-off-by: MukendiMputu <mputusam@live.com>

--- src/App.js
+++ src/App.js
@@ -19,7 +19,10 @@ class App extends React.Component {
 
   // good practice to initialize data/variable
   componentDidMount() {
-    console.log('mounted')
+    fetch()
+    .then(response => response.json())
+    .then(json => {})
+    .catch(error => {});
   }
   //
   componentDidUpdate( ){
@@ -39,15 +42,15 @@ class App extends React.Component {
 
     return (
       <div >
-{/*     <p style={style} className="text-large">
+        {/*<p style={style} className="text-large">
           {this.state.array[this.state.i]}, {this.state.intro[this.state.i]} <NameComponent />!
         </p>
 
         <button className="btn-success" onClick={this.changeGreeting.bind(this)}>
           {this.state.btnLabel[this.state.i]}
-        </button>
- */}
-{/*         <Form className="text-center">
+        </button>*/}
+
+        {/*<Form className="text-center">
           <Form.Group controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
             <Form.Control type="email" placeholder="Enter email" />
@@ -66,83 +69,90 @@ class App extends React.Component {
           <Button variant="primary" type="submit">
             Submit
           </Button>
-      </Form> */}
-      <Navbar bg="dark" variant="dark">
-        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-        <Nav className="mr-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#features">Features</Nav.Link>
-          <Nav.Link href="#pricing">Pricing</Nav.Link>
-        </Nav>
-        <Form inline>
-          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-          <Button variant="outline-info">Search</Button>
-        </Form>
-      </Navbar>
-      <Jumbotron>
-        <h1>{this.state.array[this.state.i]}, {this.state.intro[this.state.i]} <NameComponent />!</h1>
-        <p>
-          This is a simple hero unit, a simple jumbotron-style component for calling
-          extra attention to featured content or information.
-        </p>
-        <p>
-          <Button variant="primary" onClick={this.changeGreeting.bind(this)}>
-          {this.state.btnLabel[this.state.i]}</Button>
-        </p>
-      </Jumbotron>
-      <Carousel>
-        <Carousel.Item>
-          <Image src="/firstSlide.png" alt="First slide"/>
-          <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
+        </Form> */}
 
-        <Carousel.Item>
-          <Image src="/secondSlide.png" alt="Second slide"/>
-          <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
+        <Navbar bg="dark" variant="dark">
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+          <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button variant="outline-info">Search</Button>
+          </Form>
+        </Navbar>
+        <Jumbotron>
+          <h1>{this.state.array[this.state.i]}, {this.state.intro[this.state.i]} <NameComponent />!</h1>
+          <p>
+            This is a simple hero unit, a simple jumbotron-style component for calling
+            extra attention to featured content or information.
+          </p>
+          <p>
+            <Button variant="primary" onClick={this.changeGreeting.bind(this)}>
+            {this.state.btnLabel[this.state.i]}</Button>
+          </p>
+        </Jumbotron>
+        <Carousel>
+          <Carousel.Item>
+            <Image src="/firstSlide.png" alt="First slide"/>
+            <Carousel.Caption>
+              <h3>First slide label</h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
 
-        <Carousel.Item>
-          <Image src="/thirdSlide.png" alt="Third slide"/>
-          <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
+          <Carousel.Item>
+            <Image src="/secondSlide.png" alt="Second slide"/>
+            <Carousel.Caption>
+              <h3>Second slide label</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
 
-      </Carousel>
-      <br/>
-      <br/>
-      <Container>
-        <Row className=" justify-content-md-start">
-          <Col xs={6} md={4}>
-            {/* <Image src="holder.js/171x180" rounded /> */}
-            <h3>First column</h3>
-            <p>This is a simple hero unit, a simple jumbotron-style component for calling
-            extra attention to featured content or information.</p>
-            <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
-          </Col>
-          <Col xs={6} md={4}>
-            {/* <Image src="holder.js/171x180" roundedCircle /> */}
-            <h3>Second column</h3>
-            <p>This is a simple hero unit, a simple jumbotron-style component for calling
-            extra attention to featured content or information.</p>
-            <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
-          </Col>
-          <Col xs={6} md={4}>
-            {/* <Image src="holder.js/171x180" thumbnail /> */}
-            <h3>Third column</h3>
-            <p>This is a simple hero unit, a simple jumbotron-style component for calling
-            extra attention to featured content or information.</p>
-            <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
-          </Col>
-        </Row>
-      </Container>
+          <Carousel.Item>
+            <Image src="/thirdSlide.png" alt="Third slide"/>
+            <Carousel.Caption>
+              <h3>Third slide label</h3>
+              <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+
+        </Carousel>
+        <br/>
+        <br/>
+        <Container>
+          <Row className=" justify-content-md-start">
+            <Col xs={6} md={4}>
+              {/* <Image src="holder.js/171x180" rounded /> */}
+              <h3>First column</h3>
+              <p>This is a simple hero unit, a simple jumbotron-style component for calling
+              extra attention to featured content or information.</p>
+              <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+            </Col>
+            <Col xs={6} md={4}>
+              {/* <Image src="holder.js/171x180" roundedCircle /> */}
+              <h3>Second column</h3>
+              <p>This is a simple hero unit, a simple jumbotron-style component for calling
+              extra attention to featured content or information.</p>
+              <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+            </Col>
+            <Col xs={6} md={4}>
+              {/* <Image src="holder.js/171x180" thumbnail /> */}
+              <h3>Third column</h3>
+              <p>This is a simple hero unit, a simple jumbotron-style component for calling
+              extra attention to featured content or information.</p>
+              <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+            </Col>
+          </Row>
+        </Container>
+        <hr/>
+        <div className="footer mt-auto py-3">
+          <div class="container">
+            <span class="text-muted">&copy;{new Date().getFullYear()} - <NameComponent/></span>
+          </div>
+        </div>
       </div>
     );
 
